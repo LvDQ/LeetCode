faed81ed72ac9c8bc6369a78d643f6d563fdf31d
LvDQ <ky935@nyu.edu>
2018-09-30 01:07:13 -0400

    Leetcode Contest 104 - 414/3579

---
+++ 915_Partition_Array_into_Disjoint_Intervals.cpp
@@ -0,0 +1,50 @@
+Given an array A, partition it into two (contiguous) subarrays left and right so that:
+
+Every element in left is less than or equal to every element in right.
+left and right are non-empty.
+left has the smallest possible size.
+Return the length of left after such a partitioning.  It is guaranteed that such a partitioning exists.
+
+ 
+
+Example 1:
+
+Input: [5,0,3,8,6]
+Output: 3
+Explanation: left = [5,0,3], right = [8,6]
+Example 2:
+
+Input: [1,1,1,0,6,12]
+Output: 4
+Explanation: left = [1,1,1,0], right = [6,12]
+ 
+
+Note:
+
+2 <= A.length <= 30000
+0 <= A[i] <= 10^6
+It is guaranteed there is at least one way to partition A as described.
+
+
+Difficulty: medium
+
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& A) {
+        vector<int>B,C;
+        int mx=INT_MIN,mn=INT_MAX;
+        int n=A.size();
+        for(int i=0;i<n;i++){
+            mx=max(A[i],mx);
+            mn=min(A[n-i-1],mn);
+            B.push_back(mx);
+            C.push_back(mn);
+            // cout<<mx<<" "<<mn<<endl;
+        }
+        reverse(C.begin(),C.end());
+        for(int i=0;i<n-1;i++){
+            // cout<<B[i]<<" "<<C[i]<<endl;
+            if(B[i]<=C[i+1])return i+1;
+        }
+    }
+};
